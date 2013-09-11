48b2c0a234ddd063327e035514e35e8db2cff977
fakhruz85 <fakhruz@amtis.com.my>
2013-09-11 10:41:49 +0800

    initial ci folder

---
+++ system/language/english/form_validation_lang.php
@@ -0,0 +1,33 @@
+<?php
+/* Modification Log : 29072013 by Mior - tukar bi ke bm 
+ * 					  21 Ogos 2013 by Mior - tambah validate untuk phone	
+ * */
+
+$lang['required']			= "Ruangan %s diperlukan.";
+$lang['isset']				= "Ruangan %s mesti mempunyai nilai.";
+$lang['valid_email']		= "Ruangan %s mesti mengandungi alamat emel yang sah.";
+$lang['valid_emails']		= "Ruangan %s mesti mengandungi semua alamat emel yang sah.";
+$lang['valid_url']			= "Ruangan %s mesti mengandungi URL yang sah.";
+$lang['valid_ip']			= "Ruangan %s mesti mengandungi IP yang sah.";
+$lang['min_length']			= "Ruangan %s mesti sekurang-kurangnya mempunyai %s bilangan huruf.";
+$lang['max_length']			= "Ruangan %s mesti tidak melebihi %s bilangan huruf.";
+$lang['exact_length']		= "The %s field must be exactly %s characters in length.";
+$lang['alpha']				= "The %s field may only contain alphabetical characters.";
+$lang['alpha_numeric']		= "The %s field may only contain alpha-numeric characters.";
+$lang['alpha_dash']			= "The %s field may only contain alpha-numeric characters, underscores, and dashes.";
+$lang['phone']				= "Ruangan %s mesti di isi dengan nombor telefon yang sah sahaja.";
+$lang['numeric']			= "Ruangan %s mesti di isi dengan nombor sahaja.";
+$lang['is_numeric']			= "The %s field must contain only numeric characters.";
+$lang['integer']			= "The %s field must contain an integer.";
+$lang['regex_match']		= "The %s field is not in the correct format.";
+$lang['matches']			= "The %s field does not match the %s field.";
+$lang['is_unique'] 			= "The %s field must contain a unique value.";
+$lang['is_natural']			= "The %s field must contain only positive numbers.";
+$lang['is_natural_no_zero']	= "The %s field must contain a number greater than zero.";
+$lang['decimal']			= "The %s field must contain a decimal number.";
+$lang['less_than']			= "The %s field must contain a number less than %s.";
+$lang['greater_than']		= "The %s field must contain a number greater than %s.";
+
+
+/* End of file form_validation_lang.php */
+/* Location: ./system/language/english/form_validation_lang.php */
